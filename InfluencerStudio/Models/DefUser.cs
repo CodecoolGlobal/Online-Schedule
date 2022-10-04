b19d162d88e62c521038a6dfa18e15abf07f5334
Bea Gecsei <gecseibeata34@gmail.com>
2022-10-04 11:33:12 +0200

    add default accounts

---
+++ InfluencerStudio/Models/DefUser.cs
@@ -0,0 +1,7 @@
+﻿namespace InfluencerStudio.Models
+{
+    public class DefUser
+    {
+        static readonly string[] _accounts = { "Gmail, Youtube, Facebook, Instagram[apiusertest7@gmail.com, 777testuser]", "Twitter[fromGoogle]" };
+    }
+}
