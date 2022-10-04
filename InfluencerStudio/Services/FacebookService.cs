25f656b63137c96cb95ae7430c1fc6cb69cbaccf
nagyD88 <oliverdikker@gmail.com>
2022-10-04 12:23:47 +0200

    create FacebookService

---
+++ InfluencerStudio/Services/FacebookService.cs
@@ -0,0 +1,47 @@
+﻿using InfluencerStudio.Models;
+
+namespace InfluencerStudio.Services
+{
+    public interface IFacebookService
+    {
+        Task<FacebookAccount> GetAccountAsync(string accessToken);
+        Task PostOnWallAsync(string accessToken, string message);
+    }
+
+    public class FacebookService : IFacebookService
+    {
+        private readonly IFacebookClient _facebookClient;
+
+        public FacebookService(IFacebookClient facebookClient)
+        {
+            _facebookClient = facebookClient;
+        }
+
+        public async Task<FacebookAccount> GetAccountAsync(string accessToken)
+        {
+            var result = await _facebookClient.GetAsync<dynamic>(
+                accessToken, "me", "fields=id,name,email,first_name,last_name,age_range,birthday,gender,locale");
+
+            if (result == null)
+            {
+                return new FacebookAccount();
+            }
+
+            var account = new FacebookAccount
+            {
+                Id = result.id,
+                Email = result.email,
+                Name = result.name,
+                UserName = result.username,
+                FirstName = result.first_name,
+                LastName = result.last_name,
+                Locale = result.locale
+            };
+
+            return account;
+        }
+
+        public async Task PostOnWallAsync(string accessToken, string message)
+            => await _facebookClient.PostAsync(accessToken, "me/feed", new { message });
+    }
+}
