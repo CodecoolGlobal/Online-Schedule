56f313f06e17cc100c20c4f1c5d90ad79f324a04
nagyD88 <oliverdikker@gmail.com>
2022-10-04 13:02:05 +0200

    save a try accses token

--- InfluencerStudio/Models/DefUser.cs
+++ InfluencerStudio/Models/DefUser.cs
@@ -3,5 +3,6 @@
     public class DefUser
     {
         static readonly string[] _accounts = { "Gmail, Youtube, Facebook, Instagram[apiusertest7@gmail.com, 777testuser]", "Twitter[fromGoogle]" };
+        public string Token { get; } = "EAAIz88N8Bt4BAG9OHM3sqiVSLCaVizXSCQgG4vf2UZCZBwi80DU2e6tY8AkkdW1BC1ogPYiq19QAcgBnm0HWxtUKvQF8n7HRAHLMfeZCAGRd7ZCnYtLSN3QSvppiNZC7ZCxh9zb7Tn7t96AEHELTlZBlnKr1mGmquyNMpadoGMNHZCvfo98Vo9hrZCnUrw9QYObZCcH3tjrvgSXG1jxXVmce9VFnZCgIcn9eaYrIZAaDJZAElhpiM4zeTzZBlAS2ZBfwuxg3CIZD"; 
     }
 }
