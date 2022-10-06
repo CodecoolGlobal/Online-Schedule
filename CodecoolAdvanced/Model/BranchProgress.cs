1cac1cdf868d1285308b20e41fd897aba8568d23
nagyD88 <oliverdikker@gmail.com>
2022-10-06 13:27:24 +0200

    create SetProgress method

--- CodecoolAdvanced/Model/BranchProgress.cs
+++ CodecoolAdvanced/Model/BranchProgress.cs
@@ -1,21 +1,50 @@
 using System;
 
-namespace CodecoolAvence.Model {
-	public class BranchProgress {
-		public BranchProgress(Branch branch) 
-		{
-			Branch = branch;
-
-		}
-		public Branch Branch { get; }
-		public string Progresess { get; private set; }
-		private int _actualWeek;
-
-		public void SetProgress()
+namespace CodecoolAvence.Model
+{
+    public class BranchProgress
+    {
+        public BranchProgress(Branch branch)
         {
-			throw new NotImplementedException(); // mentem ebédelni cya
+            Branch = branch;
+            ActualWeek = 1;
         }
-	
-	}
+        public Branch Branch { get; }
+        public string Progresess { get; private set; }
+        public int ActualWeek { get; set; }
+
+        public void AutoSetActualWeek()
+        {
+            ActualWeek++;
+        }
+        public void SetProgress()
+        {
+            if (Branch == Branch.C_sharp)
+            {
+                Progresess = ProgressLists.Instance.cSharp[ActualWeek];
+            }
+            else if (Branch == Branch.Java)
+            {
+                Progresess = ProgressLists.Instance.java[ActualWeek];
+            }
+            else if (Branch == Branch.PHP)
+            {
+                Progresess = ProgressLists.Instance.php[ActualWeek];
+            }
+            else if (Branch == Branch.PHP)
+            {
+                Progresess = ProgressLists.Instance.php[ActualWeek];
+            }
+            else if (Branch == Branch.DevOps)
+            {
+                Progresess = ProgressLists.Instance.devOps[ActualWeek];
+            }
+            else if (Branch == Branch.TestAutomation)
+            {
+                Progresess = ProgressLists.Instance.TestAutomation[ActualWeek];
+            }
+        }
+
+    }
 
 }
