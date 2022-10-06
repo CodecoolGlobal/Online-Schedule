d1bb8f8d123001764dba723dc62cedc9c07ab0be
nagyD88 <oliverdikker@gmail.com>
2022-10-06 13:15:40 +0200

    create constructor

--- CodecoolAdvanced/Model/Student.cs
+++ CodecoolAdvanced/Model/Student.cs
@@ -1,8 +1,14 @@
 using System;
 
 namespace CodecoolAvence.Model {
-	public class Student : User  {
-		public BranchProgress BranchProgress { get; set; }
+    public class Student : User
+    {
+        public Student(string name, Branch branch) : base(name)
+        {
+            BranchProgress = new BranchProgress(branch);
+        }
+
+        public BranchProgress BranchProgress { get; set; }
 
 		public void AutoSetCurrentWeek() {
 			throw new System.NotImplementedException("Not implemented");
