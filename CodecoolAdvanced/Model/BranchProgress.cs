b2c5a6d55ebf1bc6713710f6a37aa2aeb2d6c634
nagyD88 <oliverdikker@gmail.com>
2022-10-06 13:05:34 +0200

    create progress to string

--- CodecoolAdvanced/Model/BranchProgress.cs
+++ CodecoolAdvanced/Model/BranchProgress.cs
@@ -2,10 +2,19 @@ using System;
 
 namespace CodecoolAvence.Model {
 	public class BranchProgress {
+		public BranchProgress(Branch branch) 
+		{
+			Branch = branch;
+
+		}
 		public Branch Branch { get; }
-		public Progress Progresess { get; private set; }
+		public string Progresess { get; private set; }
 		private int _actualWeek;
 
+		public void SetProgress()
+        {
+			throw new NotImplementedException(); // mentem ebédelni cya
+        }
 	
 	}
 
