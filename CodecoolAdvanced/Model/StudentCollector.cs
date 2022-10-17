0e65d2df768e234bba625e638323c894ed2f59fd
nagyD88 <oliverdikker@gmail.com>
2022-10-17 16:26:19 +0200

    create singelton student collector

---
+++ CodecoolAdvanced/Model/StudentCollector.cs
@@ -0,0 +1,27 @@
+﻿using CodecoolAvence.Model;
+
+namespace CodecoolAdvanced.Model
+{
+    public class StudentCollector
+    {
+		private static StudentCollector instance = null;
+
+		private HashSet<Student> _students;
+		private StudentCollector()
+		{
+			_students = new HashSet<Student>();
+		}
+
+		public static StudentCollector Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new StudentCollector();
+				}
+				return instance;
+			}
+		}
+	}
+}
