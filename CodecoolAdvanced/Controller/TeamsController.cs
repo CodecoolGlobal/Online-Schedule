7a2e1b84520a24080ea988ce730e61c0f2d103f9
nagyD88 <oliverdikker@gmail.com>
2022-10-17 15:13:23 +0200

    rename Homcontrroller to TeamsController and create get method to get all teams

--- CodecoolAdvanced/Controller/TeamsController.cs
+++ CodecoolAdvanced/Controller/TeamsController.cs
@@ -6,17 +6,21 @@ using System.Web.Helpers;
 namespace CodecoolAdvanced.Controller
 {
     [ApiController]
-    [Route("api")]
-    public class HomeController : ControllerBase
+    [Route("api/teams")]
+    public class TeamsController : ControllerBase
     {
         [HttpGet]
-        [Route("/teams/actual")]
+        [Route("actual")]
         public ActionResult<HashSet<Team>> GetTeamsForActualWeek()
         {
             HashSet<Team> actualTeams = TeamCollector.Instance.GetCurrentWeekTeam();
             return Ok(actualTeams); 
         }
+        [HttpGet]
+        public ActionResult<HashSet<Team>> GetAllTeams()
+        {
+            HashSet<Team> allteams = TeamCollector.Instance.;
+        }
 
-     
     }
 }
