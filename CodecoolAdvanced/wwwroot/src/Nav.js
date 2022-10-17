22eb03905d51994d5d293c4e486575b9c940ef05
nagyD88 <oliverdikker@gmail.com>
2022-10-17 14:47:37 +0200

    create links for the basic sceleton

--- CodecoolAdvanced/wwwroot/src/Nav.js
+++ CodecoolAdvanced/wwwroot/src/Nav.js
@@ -8,6 +8,9 @@ const Nav = () => {
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="teams">Teams</Link></li>
+            <li><Link to="demos">Demos</Link></li>
+            <li><Link to="jobhunters">Job Hunters</Link></li>
+            <li><Link to="interviewprep">InterviewPrep</Link></li>
             <li><Link to="about">About</Link></li>
         </ul>
     </nav>
