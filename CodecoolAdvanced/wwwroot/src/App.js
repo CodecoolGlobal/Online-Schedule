135d4dfc8f5ab9b5cb74ee0d75e7c4e4710f7c7e
nagyD88 <oliverdikker@gmail.com>
2022-10-17 14:45:02 +0200

    create Basic sceleton of rooting

--- CodecoolAdvanced/wwwroot/src/App.js
+++ CodecoolAdvanced/wwwroot/src/App.js
@@ -2,8 +2,11 @@ import About from "./About";
 import Missing from "./Missing";
 import Layout from "./Layout";
 import Teams from "./Teams";
+import Team from "./Team";
 import Home from "./Home";
 import Demos from "./Demos";
+import JobHunters from "./JobHunters";
+import InterviewPrep from "./InterviewPrep";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
@@ -15,11 +18,15 @@ function App() {
         <Route index element={<Home />} />
         <Route path="teams">
           <Route index element={<Teams />} />
+          <Route path=":id" element={<Team/>} />
         </Route>
         <Route path="demos">
           <Route index element={<Demos />} />
         </Route>
-        
+        <Route path="jobhunters">
+          <Route index element={<JobHunters />} />
+        </Route>
+        <Route path="interviewprep" element={<InterviewPrep />} />
         <Route path="about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Route>
