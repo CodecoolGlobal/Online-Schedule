872af9d79901182e1f34db082bb5060fdfbdd63b
nagyD88 <oliverdikker@gmail.com>
2022-10-17 14:33:14 +0200

    create basic app js with basic rooting

--- CodecoolAdvanced/wwwroot/src/App.js
+++ CodecoolAdvanced/wwwroot/src/App.js
@@ -1,11 +1,24 @@
+import About from "./About";
+import Missing from "./Missing";
+import Layout from "./Layout";
+import Teams from "./Teams";
+import Home from "./Home";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="teams">
+          <Route index element={<Teams />} />
+        </Route>
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
