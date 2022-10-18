0a5fae19829793b4c4cfada3d246f6b0bcb24870
nagyD88 <oliverdikker@gmail.com>
2022-10-18 10:07:43 +0200

    name refactored to be a property

--- CodecoolAdvanced/Model/User.cs
+++ CodecoolAdvanced/Model/User.cs
@@ -2,24 +2,20 @@ using System;
 
 namespace CodecoolAvence.Model {
 	public abstract class User {
-		private string _name;
+		public string Name { get; set; }
         private static int studentCounter = 0;
-        private string name;
+        
 
         public string Email { get; }
         public int Id { get; private set; }
         protected User(string name, string email)
         {
             Email = email;
-            _name = name;
+            Name = name;
             Id = studentCounter;
             studentCounter++;
         }
 
-        protected User(string name)
-        {
-            this.name = name;
-        }
     }
 
 }
