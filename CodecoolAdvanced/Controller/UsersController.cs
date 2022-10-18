4e30a43bed792e354983e09f62f825f277092467
nagyD88 <oliverdikker@gmail.com>
2022-10-18 08:54:54 +0200

    create User Controller

---
+++ CodecoolAdvanced/Controller/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodecoolAdvanced.Controller
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
