21df0421c0996e4bebe20826014d28c5817326c8
nagyD88 <oliverdikker@gmail.com>
2022-10-18 15:07:02 +0200

    create aa working fetch and first look of the teams

--- CodecoolAdvanced/wwwroot/src/Teams.js
+++ CodecoolAdvanced/wwwroot/src/Teams.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import useAxiosFetch from './hooks/useAxiosFetch'
+import { Link } from 'react-router-dom';
 const Teams = () => {
+  let url='https://localhost:7086/api/teams'
+  const { data, fetchError, isLoading } = useAxiosFetch(url
+    
+  );
+  console.log(data)
   return (
-    <div>Teams</div>
+    <>
+    {data.map(team => <div key={team.id} ><Link to={`/teams/${team.id}`}>{team.name}</Link></div>)
+
+    }
+    </>
   )
 }
 
