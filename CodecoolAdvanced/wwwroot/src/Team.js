0aeb276923a70769373d250c5f81f035cf7f7260
nagyD88 <oliverdikker@gmail.com>
2022-10-19 10:00:18 +0200

    fix map to work with loading problems

--- CodecoolAdvanced/wwwroot/src/Team.js
+++ CodecoolAdvanced/wwwroot/src/Team.js
@@ -6,15 +6,17 @@ const Team = () => {
   const { id } = useParams();
   let url=`https://localhost:7086/api/teams/${id}`
   const { data, fetchError, isLoading } = useAxiosFetch(url);
-  console.log(data.students)
+  console.log(data)
+
   return (
-  <>  {isLoading && <p className='statusMsg'>Loading posts...</p>}
+  <>  {isLoading && <p className='statusMsg'>Loading ...</p>}
       {!isLoading && fetchError && <p className='statusMsg' style={{color: "red"}}>{fetchError}</p>}
-      {!isLoading && !fetchError &&<>
+      {!isLoading && !fetchError && <>
               <h1>{data.name}</h1>
             <div>Mentor: {data.mentor}</div>
             <ul>Students: 
-                {data.students.forEach(element => {<li>{element.name}</li>})}
+                {data.students?.map(student=> 
+                  <li key={student.id}>{student.name}</li>)}
             </ul></>}
           </>
   )
