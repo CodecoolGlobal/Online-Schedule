f46bc352ad2511ae0f641b68cd62b60a9c6486bb
nagyD88 <oliverdikker@gmail.com>
2022-10-19 14:08:03 +0200

    create basic collector for education materials

---
+++ CodecoolAdvanced/Model/EdducationMaterialColllector.cs
@@ -0,0 +1,27 @@
+﻿namespace CodecoolAdvanced.Model
+{
+    public class EdducationMaterialColllector
+    {
+		private static EdducationMaterialColllector instance = null;
+
+		private EdducationMaterialColllector()
+		{
+			_materials = new HashSet<EducationalMaterial>();
+		}
+
+		public static EdducationMaterialColllector Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new EdducationMaterialColllector();
+				}
+				return instance;
+			}
+		}
+
+
+		private HashSet<EducationalMaterial> _materials;
+	}
+}
