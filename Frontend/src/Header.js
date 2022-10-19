f29b4bd72caf71f6787b9a7516956a08beb05cb8
Bea Gecsei <gecseibeata34@gmail.com>
2022-10-19 14:01:15 +0200

    basic css

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./App.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div class="header"><div class="logo">Codecool Advanced</div><div class="log"><Link to="login">login</Link></div></div>
   )
 }
 
--- Frontend/src/Nav.js
+++ Frontend/src/Nav.js
@@ -4,17 +4,32 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return ( 
     
-    <nav className='nav'>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="teams">Teams</Link></li>
-            <li><Link to="demos">Demos</Link></li>
-            <li><Link to="jobhunters">Job Hunters</Link></li>
-            <li><Link to="interviewprep">InterviewPrep</Link></li>
-            <li><Link to="about">About</Link></li>
-        </ul>
+    <nav className='sideNav'>
+          <div class="navElement active"><Link to="/">Home</Link></div>
+          <div class="navElement"><Link to="teams">Teams</Link></div>
+          <div class="navElement"><Link to="demos">Demos</Link></div>
+          <div class="navElement"><Link to="jobhunters">Job Hunters</Link></div>
+          <div class="navElement"><Link to="interviewprep">InterviewPrep</Link></div>
+          <div class="navElement"><Link to="about">About</Link></div>
     </nav>
   )
 }
+
+function NavBar(){
+  var btnContainer = document.getElementsByClassName("sideNav")[0];
+  if(btnContainer !== null){
+    var btns = btnContainer.getElementsByClassName("navElement");
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
+  }
+  
+}
+
+NavBar();
 
 export default Nav
