d557decb80c98d0383dd942383ad82b414f7ae16
nagyD88 <oliverdikker@gmail.com>
2022-10-19 14:04:57 +0200

    create Basic elments

--- CodecoolAdvanced/Model/EducationalMaterial.cs
+++ CodecoolAdvanced/Model/EducationalMaterial.cs
@@ -2,5 +2,13 @@
 {
     public class EducationalMaterial
     {
+        public EducationalMaterial(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public HashSet<string> Material { get; set; }
+
     }
 }
