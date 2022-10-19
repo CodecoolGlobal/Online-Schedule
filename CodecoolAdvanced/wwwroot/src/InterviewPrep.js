c3b76f3b61d89ac6e8f6787fe0fced82ca2ed4fa
nagyD88 <oliverdikker@gmail.com>
2022-10-19 16:34:57 +0200

    interviewprep can show thames and links to them

--- CodecoolAdvanced/wwwroot/src/InterviewPrep.js
+++ CodecoolAdvanced/wwwroot/src/InterviewPrep.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
 
 const InterviewPrep = () => {
+  let url = 'https://localhost:7086/api/material';
+  const { data, fetchError, isLoading } = useAxiosFetch(url);
+  console.log(data);
   return (
-    <div>InterviewPrep</div>
-  )
-}
+    <>
+      <ul>
+        {data?.map((material) => (
+          <li key={material.id}>
+            {material.name}
+            {material.material !== [] ? (
+              <ul>
+                {material.material.map((submaterial) => (
+                  <li>
+                    <a href={submaterial}>{submaterial}</a>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <></>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default InterviewPrep
+export default InterviewPrep;
