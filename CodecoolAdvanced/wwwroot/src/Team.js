9d8820ccd91e880afa527599de53a300a1c4bd0c
nagyD88 <oliverdikker@gmail.com>
2022-10-20 11:18:44 +0200

    try to set time

--- CodecoolAdvanced/wwwroot/src/Team.js
+++ CodecoolAdvanced/wwwroot/src/Team.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router';
 import useAxiosFetch from './hooks/useAxiosFetch';
+import api from './api/api';
 
 const Team = () => {
   const { id } = useParams();
   let url = `https://localhost:7086/api/teams/${id}`;
   const { data, fetchError, isLoading } = useAxiosFetch(url);
   console.log(data);
+
+  const [siStart, setSiStart] = useState('');
+  const [siFinish, setSiFinish] = useState('');
+  const [twStart, setTwStart] = useState('');
+  const [twFinish, setTwFinish] = useState('');
+
+  const handleSiStart = async (e) => {
+    e.preventDefault();
+    try {
+      const review = { siReviewStart: siStart };
+      const response = await api.put(`/teams/${id}/si/start`, review);
+      setSiStart('');
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+      
+    }
+  };
 
   return (
     <>
@@ -36,10 +60,16 @@ const Team = () => {
           ? 'not set yet'
           : data.siReviewStart}
       </div>
-      <form>
+      <form onSubmit={handleSiStart}>
         <label>
-        SI review start:
-          <input type="text" name="name" />
+          SI review start:
+          <input
+            id="siStart"
+            type="text"
+            required
+            value={siStart}
+            onChange={(e) => setSiStart(e.target.value)}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -47,11 +77,11 @@ const Team = () => {
         SI review finish:{' '}
         {data.siReviewFinish === '0001-01-01T00:00:00'
           ? 'not set yet'
-          : data.siReviewStart}
+          : data.siReviewFinish}
       </div>
       <form>
         <label>
-        SI review finish:
+          SI review finish:
           <input type="text" name="name" />
         </label>
         <input type="submit" value="Submit" />
@@ -60,11 +90,11 @@ const Team = () => {
         TW review start:{' '}
         {data.twReviewStart === '0001-01-01T00:00:00'
           ? 'not set yet'
-          : data.siReviewStart}
+          : data.twReviewStart}
       </div>
       <form>
         <label>
-        TW review start:
+          TW review start:
           <input type="text" name="name" />
         </label>
         <input type="submit" value="Submit" />
@@ -73,11 +103,11 @@ const Team = () => {
         TW review finish:{' '}
         {data.twReviewFinish === '0001-01-01T00:00:00'
           ? 'not set yet'
-          : data.siReviewStart}
+          : data.twReviewFinish}
       </div>
       <form>
         <label>
-        TW review finish:
+          TW review finish:
           <input type="text" name="name" />
         </label>
         <input type="submit" value="Submit" />
