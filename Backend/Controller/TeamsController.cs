2d69035cdebdd62cd8350d37bafdd11e2d858a06
Bea Gecsei <gecseibeata34@gmail.com>
2022-11-02 15:45:13 +0100

    one page for times

--- Backend/Controller/TeamsController.cs
+++ Backend/Controller/TeamsController.cs
@@ -86,58 +86,65 @@ namespace CodecoolAdvanced.Controller
             team.Name = newName;
             return NoContent();
         }
-        
+
         [HttpPut]
-        [Route("{id}/tw/start")]
-        public ActionResult ChangeTimeOfTwReviewStart(int id ,string twReviewStart)
+        [Route("{id}/review")]
+        public ActionResult ChangeTimeOfTwReviewStart(int id, string reviewTime, string type)
         {
             Team team = TeamCollector.Instance.GetTeamById(id);
             if (team == null)
             {
                 return NotFound();
             }
-            team.TwReviewStart =twReviewStart;
+            if(type == "siStart")
+            team.SiReviewStart = reviewTime;
+            else if(type == "siFinish")   
+            team.SiReviewFinish = reviewTime;
+            else if(type == "twStart")   
+            team.TwReviewStart = reviewTime;
+            else
+            team.TwReviewFinish = reviewTime;
             return NoContent();
         }
 
-        [HttpPut]
-        [Route("{id}/tw/finish")]
-        public ActionResult ChangeTimeOfTwReviewFinish(int id, string twReviewFinish)
-        {
-            Team team = TeamCollector.Instance.GetTeamById(id);
-            if (team == null)
-            {
-                return NotFound();
-            }
-            team.TwReviewFinish = twReviewFinish;
-            return NoContent();
-        }
+        //[HttpPut]
+        //[Route("{id}/tw/finish")]
+        //public ActionResult ChangeTimeOfTwReviewFinish(int id, string twReviewFinish)
+        //{
+        //    Team team = TeamCollector.Instance.GetTeamById(id);
+        //    if (team == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    team.TwReviewFinish = twReviewFinish;
+        //    return NoContent();
+        //}
 
-        [HttpPut]
-        [Route("{id}/si/start")]
-        public ActionResult ChangeTimeOfSiReviewStart(int id, string siReviewStart)
-        {
-            Team team = TeamCollector.Instance.GetTeamById(id);
-            if (team == null)
-            {
-                return NotFound();
-            }
-            team.SiReviewStart = siReviewStart;
-            return NoContent();
-        }
+        //[HttpPut]
+        //[Route("{id}/si/start")]
+        //public ActionResult ChangeTimeOfSiReviewStart(int id, string siReviewStart)
+        //{
+        //    Team team = TeamCollector.Instance.GetTeamById(id);
+        //    if (team == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    team.SiReviewStart = siReviewStart;
+        //    return NoContent();
+        //}
 
-        [HttpPut]
-        [Route("{id}/si/finish")]
-        public ActionResult ChangeTimeOfSiReviewFinish(int id, string siReviewFinish)
-        {
-            Team team = TeamCollector.Instance.GetTeamById(id);
-            if (team == null)
-            {
-                return NotFound();
-            }
-            team.SiReviewFinish = siReviewFinish;
-            return NoContent();
-        }
+        //[HttpPut]
+        //[Route("{id}/si/finish")]
+        //public ActionResult ChangeTimeOfSiReviewFinish(int id, string siReviewFinish)
+        //{
+        //    Team team = TeamCollector.Instance.GetTeamById(id);
+        //    if (team == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    team.SiReviewFinish = siReviewFinish;
+        //    return NoContent();
+        //}
 
         [HttpPut]
         [Route("{id}/add/{studentid}")]
