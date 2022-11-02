08727d84920b888798a192d8b425c4210246dde5
nagyD88 <oliverdikker@gmail.com>
2022-11-02 14:10:12 +0100

    our logo in place

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 const Header = () => {
   return (
-    <div className="header"><div className="logo"><img src='../public/logo192.png'></img>Codecool Advanced</div><div className="log"><Link to="login">login</Link></div></div>
+    <div className="header"><div className="logo"><img src='/SeeSharp_logo.png' width="40" ></img>Codecool Advanced</div><div className="log"><Link to="login">login</Link></div></div>
   )
 }
 
