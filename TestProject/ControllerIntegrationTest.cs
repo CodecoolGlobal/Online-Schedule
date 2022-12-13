8af16df6112365698a0b4e4c358f4f2ddf228794
nagyD88 <oliverdikker@gmail.com>
2022-12-13 14:17:22 +0100

    test for educationmaterial post, put

--- TestProject/ControllerIntegrationTest.cs
+++ TestProject/ControllerIntegrationTest.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using CodecoolAdvanced.Model;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,6 +16,12 @@ namespace TestProject
         {
             _factory = factory;
         }
+    
+        private JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+     
 
         [Theory]
         [InlineData("api/material")]
@@ -35,8 +45,32 @@ namespace TestProject
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("application/json; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task Post_Api_MaterialReturnSuccessAndCorrectContentType()
+        {
+            HttpContent content = new StringContent("");
+            var client = _factory.CreateClient();
+            var response =await client.PostAsync("api/material?name=alma", content);
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            string json = await response.Content.ReadAsStringAsync();
+            EducationalMaterial eduMat = JsonSerializer.Deserialize<EducationalMaterial>(json, options)!; 
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+            Assert.Equal("alma", eduMat.Name);
+        }
+        [Fact]
+        public async Task Put_Api_MaterialReturnSuccessAndCorrectContentType()
+        {
+            HttpContent content = new StringContent("");
+            var client = _factory.CreateClient();
+            var response = await client.PostAsync("api/material?name=alma", content);
+            string json = await response.Content.ReadAsStringAsync();
+            EducationalMaterial eduMat = JsonSerializer.Deserialize<EducationalMaterial>(json, options)!;
+            var response2 = await client.PutAsync($"api/material/{eduMat.ID}/add?material=Tanc", content);
+            response2.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
         }
     }
 }
