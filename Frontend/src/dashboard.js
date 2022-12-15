7f6fd9eec5dd93753834df1d7535d386a118764f
SsdHUN <santhasoma1999@gmail.com>
2022-12-15 14:04:08 +0100

    remove unnesessery code

--- Frontend/src/dashboard.js
+++ Frontend/src/dashboard.js
@@ -1,34 +1,29 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 import Modal from './modal.js';
 
-const Dashboard= ({children})=> {
-
-  const [show, setShow]= useState(false);
-
-   
-    
-  
+const Dashboard = ({ children }) => {
+  const [show, setShow] = useState(false);
 
   const showModal = () => {
     setShow(true);
   };
 
   const hideModal = () => {
-    setShow(false)
+    setShow(false);
   };
-    return (
-      <>
-        <Modal show={show} handleClose={hideModal} children={children}>
-          
-        </Modal>
-        <button type="button" onClick={showModal}>
-          +
-        </button>
-      </>
-    );
-  }
+  return (
+    <>
+      <Modal
+        show={show}
+        handleClose={hideModal}
+        children={children}
+      ></Modal>
+      <button type="button" onClick={showModal}>
+        +
+      </button>
+    </>
+  );
+};
 
-
-
-export default Dashboard
+export default Dashboard;
